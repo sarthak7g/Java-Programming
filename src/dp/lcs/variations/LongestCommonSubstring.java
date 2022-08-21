cee6b713854f50a151aef7005297cc89ef873105
sarthak <gishu80@gmail.com>
2022-08-21 21:49:59 +0530

    Day 44: Longest Common Substring Recursive

--- src/dp/lcs/variations/LongestCommonSubstring.java
+++ src/dp/lcs/variations/LongestCommonSubstring.java
@@ -8,10 +8,18 @@ package dp.lcs.variations;
  *  Approach 1:
  *      <ul>
  *          <li>Iterative DP, using matrix</li>
- *          <li>Two cases char(t)=char(s)  </li>
+ *          <li>Two cases, </li>
  *          <li>char(t)==char(s), arr[i][j] = arr[i-1][j-1] + 1 </li>
  *          <li>char(t)!=char(s), arr[i][j] = 0 </li>
  *          <li>Time complexity: O(n^2)</li>
+ *      </ul>
+ *  Approach 2:
+ *      <ul>
+ *          <li>Recursive without dp</li>
+ *          <li>Two cases, </li>
+ *          <li>char(t)==char(s), f(i,j, count) = max(count, f(i+1, j+1, count+1), f(i+1, j, 0), f(i, j+1, 0)) </li>
+ *          <li>char(t)!=char(s), f(i,j, count) = max(count, f(i+1, j, 0), f(i, j+1, 0)) </li>
+ *          <li>Time complexity: O(2^n)</li>
  *      </ul>
  * </body>
  */
@@ -19,6 +27,8 @@ package dp.lcs.variations;
 public class LongestCommonSubstring {
     public static void main(String[] args) {
         System.out.println(longestCommonSubstr("abchdk", "rcabdd"));
+        System.out.println(longestCommonSubstrRecursion("abchdk", "rcabdd", 0, 0, 0));
+        System.out.println(longestCommonSubstrRecursion("abcdxyz", "xyzabcd", 0, 0, 0));
     }
     static int longestCommonSubstr(String s1, String s2){
         int n = s1.length();
@@ -34,5 +44,22 @@ public class LongestCommonSubstring {
             }
         }
         return max;
+    }
+    static int longestCommonSubstrRecursion(String a, String b, int i, int j, int count) {
+        // base case
+        if(i==a.length() || j==b.length()) {
+            return count;
+        }
+
+        int max = count;
+        // subproblem
+        if(a.charAt(i) == b.charAt(j)) {
+            max = Math.max(max, longestCommonSubstrRecursion(a, b, i+1, j+1, count+1));
+        }
+        return max(max, longestCommonSubstrRecursion(a, b, i+1, j, 0), longestCommonSubstrRecursion(a, b, i, j+1, 0));
+    }
+
+    static int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
     }
 }
