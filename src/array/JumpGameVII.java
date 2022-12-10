f664f13beac9afe4f2e137fdd97df32b636313bf
sarthak <gishu80@gmail.com>
2022-12-10 17:41:48 +0530

    [FIX] Jump Game VII

--- src/array/JumpGameVII.java
+++ src/array/JumpGameVII.java
@@ -1,5 +1,22 @@
 package array;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/jump-game-vii/description/">Jump Game VII</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Use Queue to store index. At each index where s[i] = '0', check if it is reachable by previous indexes or not.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ */
+
 public class JumpGameVII {
     public static void main(String[] args) {
         System.out.println(canReach("01", 1, 1));
@@ -8,31 +25,20 @@ public class JumpGameVII {
         System.out.println(canReach("0000000", 2, 3));
         System.out.println(canReach("011000001011110000", 4, 7));
     }
-    public static boolean canReach(String s, int min, int max) {
-//        System.out.println(s.length());
-        int st=min, end=max, ns=min, ne=max;
-        if(s.charAt(s.length()-1) == '1') return false;
-        if(end == s.length()-1) return true;
-        for(int i=0; i<s.length(); i++) {
-            if(i > end) {
-                return false;
-            }
-            if(i>=st && i<=end && s.charAt(i) == '0') {
-                if(ns == st) {
-                    ns = i+min;
-                }else {
-                    ns = Math.min(ns, i+min);
+    public static boolean canReach(String s, int minJump, int maxJump) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+
+        for(int i=minJump; i<s.length(); i++) {
+            if(s.charAt(i) == '0') {
+                while(!q.isEmpty() && q.peek()<i-maxJump) q.poll();
+
+                if(q.isEmpty()) return false;
+                if(q.peek() + minJump <= i) {
+                    q.add(i);
+                    if(i == s.length()-1) return true;
                 }
-                ne = i + max;
-
-                if(ns <= s.length()-1 && ne >= s.length()-1) return true;
-
             }
-            if(i == end && ns < s.length()) {
-                st = ns;
-                end = ne;
-            }
-//            System.out.println(i + " " + ns + " " + ne);
         }
         return false;
     }
