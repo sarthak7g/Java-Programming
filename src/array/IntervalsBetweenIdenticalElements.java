897c8af07e4e58dfd44ac1c33e4022876e6a2690
sarthak <gishu80@gmail.com>
2022-12-14 23:59:18 +0530

    Day 134: Removing Minimum Number of Magic Beans

--- src/array/IntervalsBetweenIdenticalElements.java
+++ src/array/IntervalsBetweenIdenticalElements.java
@@ -1,42 +1,65 @@
 package array;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/intervals-between-identical-elements/description/">Intervals Between Identical Elements</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint: Group indexes with arr[i] using hashMap. Now, for every list corresponding to a key in hashMap find the formula.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ */
+
 public class IntervalsBetweenIdenticalElements {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(getDistances(new int[]{2,1,3,1,2,3,3})));
+        System.out.println(Arrays.toString(getDistances(new int[]{10,5,10,10})));
+        System.out.println(Arrays.toString(getDistances(new int[]{1,2,1,2,1,3,1,2,1,3,1,2,3,3})));
     }
 
-    public long[] getDistances(int[] arr) {
+    public static long[] getDistances(int[] arr) {
         int n = arr.length;
         long[] ans = new long[n];
         Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i=0; i<n ;i++) {
+        for(int i=0; i<n; i++) {
             List<Integer> l;
             if(map.containsKey(arr[i])) {
                 l = map.get(arr[i]);
             }
-            else l = new ArrayList<Integer>();
+            else l = new ArrayList<>();
             l.add(i);
+            map.put(arr[i], l);
         }
-        System.out.println(map);
+        // System.out.println(map);
 
-        for(Entry<Integer> entry : map.entrySet()) {
-            solve(map.get(entry), ans);
+        for(Integer key : map.keySet()) {
+            solve(map.get(key), ans);
         }
 
         return ans;
     }
 
-    public long solve(List<Integer> list, long[] ans) {
+    public static void solve(List<Integer> list, long[] ans) {
         int n = list.size();
         long[] arr = new long[n+1];
 
         for(int i=0; i<n; i++) {
             arr[i+1] = arr[i] + list.get(i);
         }
-        return ans;
+
+        for(int i=0; i<n; i++) {
+            int el = list.get(i);
+            ans[el] = el * (long)(2L *i + 1 - n) - arr[i] + arr[n] - arr[i+1];
+        }
     }
 }
---
+++ src/array/RemovingMinimumNumberOfMagicBeans.java
@@ -0,0 +1,44 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/removing-minimum-number-of-magic-beans/description/">Removing Minimum Number of Magic Beans</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint: Try with O(n^2) solution.</li>
+ *      <li>Sort the array. Then, find the mathematical formula.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ */
+
+public class RemovingMinimumNumberOfMagicBeans {
+    public static void main(String[] args) {
+        System.out.println(minimumRemoval(new int[] {4,1,6,5}));
+        System.out.println(minimumRemoval(new int[] {2,10,3,2}));
+    }
+
+    public static long minimumRemoval(int[] arr) {
+        int n = arr.length;
+        long[] pref = new long[n+1];
+        Arrays.sort(arr);
+
+        for(int i=0; i<n; i++) {
+            pref[i+1] = pref[i] + arr[i];
+        }
+
+        long ans = Long.MAX_VALUE, temp;
+        for(int i=0; i<n; i++) {
+            temp = pref[i] + pref[n] - pref[i+1] - arr[i] * (long)(n-i-1);
+            // System.out.println(arr[i] + " " + temp);
+            ans = Math.min(ans, temp);
+        }
+
+        return ans;
+    }
+}
