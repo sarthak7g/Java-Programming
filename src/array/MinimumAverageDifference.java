ffd419314826700d903f65fba930269243325aa8
sarthak <gishu80@gmail.com>
2022-12-18 19:02:11 +0530

    Day 137: Number of Ways to Split Array

--- src/array/MinimumAverageDifference.java
+++ src/array/MinimumAverageDifference.java
@@ -16,27 +16,28 @@ package array;
 
 public class MinimumAverageDifference {
     public static void main(String[] args) {
-        System.out.println(minimumAverageDifference(new int[] {2,5,3,9,5,3}));
-        System.out.println(minimumAverageDifference(new int[] {0}));
+        System.out.println(minimumAverageDifference(new int[]{2, 5, 3, 9, 5, 3}));
+        System.out.println(minimumAverageDifference(new int[]{0}));
     }
+
     public static int minimumAverageDifference(int[] nums) {
         int n = nums.length, min = Integer.MAX_VALUE, temp, ans = -1;
-        long[] arr = new long[n+1];
-        for(int i=0; i<n; i++) {
-            arr[i+1] = arr[i] + nums[i];
+        long[] arr = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = arr[i] + nums[i];
         }
 
-        for(int i=0; i<n-1; i++) {
-            temp = (int)Math.abs(arr[i+1]/(i+1) - (arr[n]-arr[i+1])/(n-i-1));
-            if(temp < min) {
+        for (int i = 0; i < n - 1; i++) {
+            temp = (int) Math.abs(arr[i + 1] / (i + 1) - (arr[n] - arr[i + 1]) / (n - i - 1));
+            if (temp < min) {
                 min = temp;
                 ans = i;
             }
         }
 
-        temp = (int)Math.abs(arr[n]/n);
-        if(temp < min) {
-            ans = n-1;
+        temp = (int) Math.abs(arr[n] / n);
+        if (temp < min) {
+            ans = n - 1;
         }
         return ans;
     }
---
+++ src/array/NumberOfWaysToSplitArray.java
@@ -0,0 +1,37 @@
+package array;
+
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="Number of Ways to Split Array">Number of Ways to Split Array</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint : Use prefix sum and find mathematical equation.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ */
+
+public class NumberOfWaysToSplitArray {
+    public static void main(String[] args) {
+        System.out.println(waysToSplitArray(new int[]{2, 5, 3, 9, 5, 3}));
+        System.out.println(waysToSplitArray(new int[]{10, 4, -8, 7}));
+    }
+
+    public static int waysToSplitArray(int[] nums) {
+        int n = nums.length, ans = 0;
+        long[] arr = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = arr[i] + nums[i];
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i + 1] >= (arr[n] - arr[i + 1]))
+                ans += 1;
+        }
+
+        return ans;
+    }
+}
