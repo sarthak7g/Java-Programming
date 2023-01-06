49d2edc4895aa9aa8690016b082b5ff8fd92bc31
sarthak <gishu80@gmail.com>
2023-01-06 23:36:23 +0530

    [OPTIMIZE] Maximum White Tiles Covered by a Carpet

--- src/array/MaximumWhiteTilesCoveredByACarpet.java
+++ src/array/MaximumWhiteTilesCoveredByACarpet.java
@@ -1,44 +1,58 @@
 package array;
 
-import java.util.TreeMap;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/maximum-white-tiles-covered-by-a-carpet/description/">Maximum White Tiles Covered by a Carpet</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint: Sorting and prefix array. refer <a href="https://leetcode.com/problems/maximum-white-tiles-covered-by-a-carpet/solutions/2038674/python-explanation-with-pictures-sliding-window/">solution</a></li>
+ *      <li>Time complexity: O(n log(n))</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ * </body>
+ */
 
 public class MaximumWhiteTilesCoveredByACarpet {
     public static void main(String[] args) {
-
+        System.out.println(maximumWhiteTiles(new int[][]{{1, 5}, {10, 11}, {12, 18}, {20, 25}, {30, 32}}, 10));
+        System.out.println(maximumWhiteTiles(new int[][]{{10, 11}, {1, 1}}, 2));
     }
 
-    public int maximumWhiteTiles(int[][] tiles, int cl) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        int n = 0, sum = 0, left = 0, ans = 0, max = 0, temp;
-        boolean flag=false;
-        map.put(-1, -1);
-        for(int[] tile : tiles) {
-            n = Math.max(n, tile[1]);
-            map.put(tile[0], map.getOrDefault(tile[0], 0)+1);
-            map.put(tile[1]+1, map.getOrDefault(tile[1]+1, 0)-1);
+    public static int maximumWhiteTiles(int[][] tiles, int cl) {
+        Arrays.sort(tiles, Comparator.comparingInt(a -> a[0]));
+        int n = tiles.length, sum, left, right, max = 0, j = 0;
+        int[] prefix_arr = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix_arr[i + 1] = prefix_arr[i] + tiles[i][1] - tiles[i][0] + 1;
         }
 
-        System.out.println(map);
+        for (int i = 0; i < n; i++) {
+            left = tiles[i][0];
+            right = left + cl - 1;
 
-        // sliding window
-        for(int right=0; right<=n; right++) {
-            if(right-left > cl) {
-                Integer val = map.floorKey(left);
-                if(val != null && val != -1) {
-                    ans -= 1;
-                }
-                left++;
+            while (j < n && right > tiles[j][1]) {
+                j++;
             }
-            temp = map.getOrDefault(right, 0);
-            if(temp == -1) {
-                flag = false;
+
+            if (j == n) {
+                sum = prefix_arr[n] - prefix_arr[i];
+                max = Math.max(max, sum);
+                break;
             }
-            else if(temp == 1) {
-                flag = true;
+
+            if (right < tiles[j][0]) {
+                sum = prefix_arr[j] - prefix_arr[i];
+            } else {
+                sum = prefix_arr[j + 1] - prefix_arr[i] - (tiles[j][1] - right);
             }
-            if(flag) ans += 1;
-            System.out.println(left + " " + right + " " + ans);
-            max = Math.max(ans, max);
+
+            max = Math.max(max, sum);
         }
 
         return max;
