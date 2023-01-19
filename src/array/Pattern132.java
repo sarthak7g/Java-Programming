a569446d490154fcd7cd9e15047e7b5fdb3c04f1
sarthak <gishu80@gmail.com>
2023-01-19 23:55:36 +0530

    [FIX] 132 Pattern

--- src/array/Pattern132.java
+++ src/array/Pattern132.java
@@ -2,6 +2,23 @@ package array;
 
 import java.util.Stack;
 
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/132-pattern/description/">132 Pattern</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint: MS</li>
+ *      <li>Start from RHS, check if each element can be 3 in 132 pattern.</li>
+ *      <li>Create a min array which will save min value or 1 at each index.</li>
+ *      <li>While traversing from back we will maintain a stack, which will have the smallest element at the top.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ */
+
 public class Pattern132 {
     public static void main(String[] args) {
         System.out.println(find132pattern(new int[] {3,5,0,3,2}));
@@ -12,42 +29,20 @@ public class Pattern132 {
     }
 
     public static boolean find132pattern(int[] arr) {
-        Stack<Integer> s1 = new Stack<>(), s2 = new Stack<>();
-        int high;
-        for (int j : arr) {
-            if (s2.empty()) {
-                if (s1.empty()) {
-                    s1.push(j);
-                } else if (s1.size() == 1) {
-                    if (j <= s1.peek()) s1.pop();
-                    s1.push(j);
-                } else {
-                    high = s1.pop();
-                    if (j >= high) {
-                        s1.push(j);
-                        continue;
-                    } else if (j > s1.peek()) return true;
-                    else if (j < s1.peek()) {
-                        s2.push(j);
-                    }
-                    s1.push(high);
-                }
-            } else {
-                high = s1.pop();
-                if (j >= high) {
-                    s1.pop();
-                    s1.push(s2.pop());
-                    s1.push(j);
-                    continue;
-                } else if (j <= s2.peek()) {
-                    s2.pop();
-                    s2.push(j);
-                } else if (j > s1.peek()) {
-                    return true;
-                }
-                s1.push(high);
+        Stack<Integer> s = new Stack<>();
+        int[] min = new int[arr.length];
+        min[0] = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            min[i] = Math.min(arr[i], min[i-1]);
+        }
+
+        for(int j=arr.length-1; j>=0; j--) {
+            if(arr[j] == min[j]) continue;
+            while(!s.empty() && arr[j] > s.peek()) {
+                if(s.peek() > min[j]) return true;
+                s.pop();
             }
-//            System.out.println(i + " " + s1 + " " + s2);
+            s.push(arr[j]);
         }
         return false;
     }
