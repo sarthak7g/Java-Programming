1dac880b3b813174032778f1e15eb23857546b25
sarthak <gishu80@gmail.com>
2023-01-25 23:59:32 +0530

    [OPTIMIZE] Daily Temperatures

--- src/array/DailyTemperatures.java
+++ src/array/DailyTemperatures.java
@@ -23,16 +23,29 @@ import java.util.Stack;
  *      <li>Time complexity: O(n)</li>
  *      <li>Space complexity: O(n)</li>
  * </ul>
+ * <b>Approach 3:</b>
+ * <ul>
+ *      <li>Hint: Iterate backwards and use the answer you're constructing. Refer <a href="https://leetcode.com/problems/daily-temperatures/solutions/1516538/daily-temperatures/">solution</a></li>
+ *      <li>To further optimize it, we will use a hottest variable.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(1)</li>
+ * </ul>
+ * </body>
  */
 
 public class DailyTemperatures {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
         System.out.println(Arrays.toString(dailyTemperatures2(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
+        System.out.println(Arrays.toString(dailyTemperatures3(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
+
         System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 40, 50, 60})));
         System.out.println(Arrays.toString(dailyTemperatures2(new int[]{30, 40, 50, 60})));
+        System.out.println(Arrays.toString(dailyTemperatures3(new int[]{30, 40, 50, 60})));
+
         System.out.println(Arrays.toString(dailyTemperatures(new int[]{30, 60, 90})));
         System.out.println(Arrays.toString(dailyTemperatures2(new int[]{30, 60, 90})));
+        System.out.println(Arrays.toString(dailyTemperatures3(new int[]{30, 60, 90})));
     }
 
     public static int[] dailyTemperatures(int[] arr) {
@@ -58,7 +71,7 @@ public class DailyTemperatures {
         for (int i = 0; i < n; i++) {
             while (!s.empty() && arr[i] > arr[s.peek()]) {
                 idx = s.pop();
-                ans[idx] = i-idx;
+                ans[idx] = i - idx;
             }
             s.push(i);
         }
@@ -66,5 +79,24 @@ public class DailyTemperatures {
         return ans;
     }
 
+    public static int[] dailyTemperatures3(int[] arr) {
+        int n = arr.length, curr, hottest = arr[n - 1];
+        int[] ans = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ans[i] >= hottest) {
+                hottest = ans[i];
+                continue;
+            }
+
+            curr = 1;
+            while (ans[i + curr] != 0 && arr[i + curr] <= arr[i]) {
+                curr += ans[i + curr];
+            }
+
+            if (arr[i + curr] > arr[i]) ans[i] = curr;
+        }
+
+        return ans;
+    }
 
 }
