1622d90ac989c0123451161535ddafe3342fc563
sarthak <gishu80@gmail.com>
2023-01-29 23:59:13 +0530

    Day 167: Maximum Width Ramp

---
+++ src/array/MaximumWidthRamp.java
@@ -0,0 +1,56 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3>Level: Medium </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/maximum-width-ramp/">Maximum Width Ramp</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Hint: Use an array list to store decreasing integer values and traverse backwards linearly.</li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ * </body>
+ */
+public class MaximumWidthRamp {
+    public static void main(String[] args) {
+        System.out.println(maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5}));
+        System.out.println(maxWidthRamp(new int[]{9, 8, 1, 0, 1, 9, 4, 0, 4, 1}));
+        System.out.println(maxWidthRamp(new int[]{1, 2, 1}));
+    }
+
+    public static int maxWidthRamp(int[] nums) {
+        int n = nums.length, max = nums[0], ans = 0, curr;
+        List<Integer> l = new ArrayList<>();
+        l.add(0);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= max) {
+                max = nums[i];
+                ans = Math.max(i, ans);
+                // System.out.println(i + "  " + ans);
+                continue;
+            }
+            curr = l.size() - 1;
+            if (nums[i] < nums[l.get(curr)]) {
+                l.add(i);
+                // System.out.println(i + "  " + ans);
+                continue;
+            }
+            while (curr > 0 && nums[i] >= nums[l.get(curr)]) {
+                curr--;
+            }
+            ans = Math.max(i - l.get(curr) - 1, ans);
+            // System.out.println(i + " " + (i-l.get(curr)-1));
+        }
+        // System.out.println(l);
+
+        return ans;
+    }
+
+
+}
