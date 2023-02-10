19da92b864bee10f9322fe5c213ca62c3b264b6b
sarthak <gishu80@gmail.com>
2023-02-10 23:53:20 +0530

    [OPTIMIZE] Count Submatrices With All Ones

--- src/array/CountSubmatricesWithAllOnes.java
+++ src/array/CountSubmatricesWithAllOnes.java
@@ -1,6 +1,8 @@
 package array;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 /**
  * <h3>Level: Medium </h3>
@@ -8,10 +10,16 @@ import java.util.Arrays;
  * refer to: <a href="https://leetcode.com/problems/count-submatrices-with-all-ones/description/">Count Submatrices With All Ones</a>
  * <br/>
  * <br/>
- * <b>Approach:</b>
+ * <b>Approach 1:</b>
  * <ul>
  *      <li>Brute force. Just try to find out solution for a 1d array and then compress each possible 2d array into a 1d array. Refer <a href="https://leetcode.com/problems/count-submatrices-with-all-ones/solutions/720265/java-detailed-explanation-from-o-mnm-to-o-mn-by-using-stack/">solution</a></li>
  *      <li>Time complexity: O(n^3)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ * <b>Approach 2:</b>
+ * <ul>
+ *      <li>MS: Increasing stack. Refer <a href="https://leetcode.com/problems/count-submatrices-with-all-ones/solutions/720265/java-detailed-explanation-from-o-mnm-to-o-mn-by-using-stack/">solution</a></li>
+ *      <li>Time complexity: O(n^2)</li>
  *      <li>Space complexity: O(n)</li>
  * </ul>
  * </body>
@@ -21,6 +29,9 @@ public class CountSubmatricesWithAllOnes {
     public static void main(String[] args) {
         System.out.println(numSubmat(new int[][]{{1, 0, 1}, {1, 1, 0}, {1, 1, 0}}));
         System.out.println(numSubmat(new int[][]{{0, 1, 1, 0}, {0, 1, 1, 1}, {1, 1, 1, 0}}));
+        System.out.println();
+        System.out.println(numSubmat2(new int[][]{{1, 0, 1}, {1, 1, 0}, {1, 1, 0}}));
+        System.out.println(numSubmat2(new int[][]{{0, 1, 1, 0}, {0, 1, 1, 1}, {1, 1, 1, 0}}));
     }
 
     public static int numSubmat(int[][] mat) {
@@ -44,6 +55,38 @@ public class CountSubmatricesWithAllOnes {
             sum = j == 0 ? 0 : sum + 1;
             res += sum;
         }
+        return res;
+    }
+
+    public static int numSubmat2(int[][] mat) {
+        int m = mat[0].length, ans = 0;
+        int[] arr = new int[m];
+        for (int[] ints : mat) {
+            for (int col = 0; col < m; col++) {
+                arr[col] = ints[col] == 0 ? 0 : arr[col] + 1;
+            }
+            ans += find(arr);
+        }
+        return ans;
+    }
+
+    public static int find(int[] arr) {
+        int res = 0, temp;
+        int[] sum = new int[arr.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.getFirst()] > arr[i])
+                stack.remove();
+
+            if (stack.isEmpty()) {
+                sum[i] += arr[i] * (i + 1);
+            } else {
+                temp = stack.getFirst();
+                sum[i] = sum[temp] + arr[i] * (i - temp);
+            }
+            stack.addFirst(i);
+        }
+        for (int s : sum) res += s;
         return res;
     }
 }
