a8c6eaa8841b381c243102efdb7f7d660dede8b5
sarthak <gishu80@gmail.com>
2023-02-21 23:58:41 +0530

    [FIX] Steps to Make Array Non-decreasing

--- src/array/MinMaxGame.java
+++ src/array/MinMaxGame.java
@@ -13,6 +13,7 @@ package array;
  * </ul>
  * </body>
  */
+
 public class MinMaxGame {
     public static void main(String[] args) {
         System.out.println(minMaxGame(new int[]{1, 3, 5, 2, 4, 8, 2, 2}));
--- src/array/StepsToMakeArrayNonDecreasing.java
+++ src/array/StepsToMakeArrayNonDecreasing.java
@@ -3,6 +3,21 @@ package array;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * <h3>Level: Easy </h3>
+ * <body>
+ * refer to: <a href="https://leetcode.com/problems/steps-to-make-array-non-decreasing/description/">Steps to Make Array Non-decreasing</a>
+ * <br/>
+ * <br/>
+ * <b>Approach:</b>
+ * <ul>
+ *      <li>Refer <a href="https://leetcode.com/problems/steps-to-make-array-non-decreasing/solutions/2085967/python-explanation-with-pictures-stack/">solution</a></li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
+ * </body>
+ */
+
 public class StepsToMakeArrayNonDecreasing {
     public static void main(String[] args) {
         System.out.println(totalSteps(new int[]{5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11}));
@@ -11,21 +26,22 @@ public class StepsToMakeArrayNonDecreasing {
     }
 
     public static int totalSteps(int[] nums) {
-        int ans = 0, el = -1;
+        int ans = 0, el;
         int[] dp = new int[nums.length];
         Deque<Integer> stack = new ArrayDeque<>();
         stack.addFirst(0);
         for(int i=1; i<nums.length; i++) {
             while(!stack.isEmpty() && nums[i] >= nums[stack.getFirst()]) {
                 el = stack.remove();
+                dp[i] = Math.max(dp[i], dp[el]+1);
             }
-            if(!stack.isEmpty()) {
-                dp[i] =  el != -1 ? dp[el] + 1 : 1;
+            if(stack.isEmpty()) dp[i] = 0;
+            else if(nums[i] < nums[i-1]) {
+                dp[i] = 1;
             }
             stack.addFirst(i);
             ans = Math.max(ans, dp[i]);
         }
-
         return ans;
     }
 }
