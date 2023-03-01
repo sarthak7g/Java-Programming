cb01a89ddd46e8017ffef544a9244e226467d8e3
sarthak <gishu80@gmail.com>
2023-03-01 23:54:53 +0530

    [OPTIMIZE] Car Fleet II

--- src/stack/CarFleetII.java
+++ src/stack/CarFleetII.java
@@ -2,6 +2,8 @@ package stack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.ArrayDeque;
 import java.util.List;
 
 /**
@@ -16,6 +18,14 @@ import java.util.List;
  *      <li>Time complexity: O(n^2)</li>
  *      <li>Space complexity: O(n)</li>
  * </ul>
+ * <b>Approach 2:</b>
+ * <ul>
+ *      <li>Intuition: While traversing from right, and maintain a stack of cars ordered by the time(decreasing).</li>
+ *      <li>We will also use the time taken by the next car to meet car ahead of it, i.e. ans[j], cz after that time(ans[j]), it becomes a part of new fleet and the speed will change.</li>
+ *      <li>Refer <a href="https://leetcode.com/problems/car-fleet-ii/solutions/1085987/java-c-python-o-n-stack-solution/">solution</a></li>
+ *      <li>Time complexity: O(n)</li>
+ *      <li>Space complexity: O(n)</li>
+ * </ul>
  * </body>
  */
 
@@ -23,6 +33,13 @@ public class CarFleetII {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(getCollisionTimes(new int[][]{{1, 2}, {2, 1}, {4, 3}, {7, 2}})));
         System.out.println(Arrays.toString(getCollisionTimes(new int[][]{{3, 4}, {5, 4}, {6, 3}, {9, 1}})));
+        System.out.println(Arrays.toString(getCollisionTimes(new int[][]{{1, 4}, {4, 5}, {7, 1}, {13, 4}, {14, 3}, {15, 2}, {16, 5}, {19, 1}})));
+        System.out.println(Arrays.toString(getCollisionTimes(new int[][]{{2, 2}, {4, 5}, {5, 5}, {8, 1}, {9, 3}, {10, 1}, {11, 4}, {12, 1}, {17, 4}, {20, 2}})));
+        System.out.println();
+        System.out.println(Arrays.toString(getCollisionTimes2(new int[][]{{1, 2}, {2, 1}, {4, 3}, {7, 2}})));
+        System.out.println(Arrays.toString(getCollisionTimes2(new int[][]{{3, 4}, {5, 4}, {6, 3}, {9, 1}})));
+        System.out.println(Arrays.toString(getCollisionTimes2(new int[][]{{1, 4}, {4, 5}, {7, 1}, {13, 4}, {14, 3}, {15, 2}, {16, 5}, {19, 1}})));
+        System.out.println(Arrays.toString(getCollisionTimes2(new int[][]{{2, 2}, {4, 5}, {5, 5}, {8, 1}, {9, 3}, {10, 1}, {11, 4}, {12, 1}, {17, 4}, {20, 2}})));
     }
 
     public static double[] getCollisionTimes(int[][] arr) {
@@ -41,14 +58,15 @@ public class CarFleetII {
                 speed = arr[i + 1][1];
             }
             // System.out.println(i + " " + dist + " " + speed + " " + time);
-            for (int j = 0; j < tmp.size(); j++) {
+            for (int j = tmp.size() - 1; j >= 0; j--) {
                 curr = tmp.get(j);
                 if (time != -1 && dist <= curr.get(0)) {
                     break;
                 }
-                if (arr[i][1] < curr.get(1)) continue;
+                if (arr[i][1] <= curr.get(1)) continue;
                 time = (curr.get(0) - arr[i][0] - (arr[i][1] * curr.get(2))) / (arr[i][1] - curr.get(1));
-                dist = (arr[i][0] + arr[i][1] * curr.get(2) + arr[i][1] * time);
+                time += curr.get(2);
+                dist = (arr[i][0] + arr[i][1] * time);
                 speed = curr.get(1);
             }
             if (time != -1) {
@@ -60,6 +78,35 @@ public class CarFleetII {
             }
             // System.out.println(tmp);
             ans[i] = time;
+        }
+
+        return ans;
+    }
+
+    public static double[] getCollisionTimes2(int[][] arr) {
+        int l = arr.length, p, s, dist, speed;
+        Deque<Integer> stack = new ArrayDeque<>();
+        double[] ans = new double[l];
+
+        for (int i = l - 1; i >= 0; i--) {
+            ans[i] = -1.0;
+            dist = arr[i][0];
+            speed = arr[i][1];
+            while (!stack.isEmpty()) {
+                int j = stack.getFirst();
+                p = arr[j][0];
+                s = arr[j][1];
+                if (speed <= s || 1.0 * (p - dist) / (speed - s) >= ans[j] && ans[j] > 0) {
+                    stack.remove();
+                } else break;
+            }
+            if (!stack.isEmpty()) {
+                int j = stack.getFirst();
+                p = arr[j][0];
+                s = arr[j][1];
+                ans[i] = 1.0 * (p - dist) / (speed - s);
+            }
+            stack.addFirst(i);
         }
 
         return ans;
